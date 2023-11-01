eaaca5931f37736f500199b065634d9f54929fb5
Vincent Mugendi <vincentmwathi5@gmail.com>
2023-11-01 00:10:36 -0700

    Task 1 Solved

--- binary_trees.h
+++ binary_trees.h
@@ -3,23 +3,20 @@
 
 #include <stddef.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-typedef struct binary_tree_s
+/* Basic Binary Tree Structure */
+struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} binary_tree_t;
+};
+typedef struct binary_tree_s binary_tree_t;
 
+/* Function Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
+
